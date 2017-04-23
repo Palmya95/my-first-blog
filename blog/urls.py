b1f6539041d31abc0df401ffc152d109b4547c58
Palmya95 <butsav@gmail.com>
2017-04-23 18:49:57 +0200

    création du formulaire d'ajout/modification

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/post_latest/$', views.post_latest, name='post_latest'),
+    url(r'^post/categories_list/$', views.category_list, name='category_list'),
+    url(r'^post/categories_posts/$', views.category_posts, name='category_posts'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post
+from .models import Post, Category
+from .forms import PostForm
+
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -9,3 +11,42 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_latest(request):
+	latest_posts_list = Post.objects.all().order_by('-published_date')[:5]
+	return render(request, 'blog/post_latest.html', {'latest_posts_list':latest_posts_list})
+
+def category_list(request):
+	liste_categories = Category.objects.all()
+	return render(request, 'blog/post_latest.html', {'liste_categories':liste_categories})
+
+def category_posts(request):
+	posts_categorie1 = Post.objects.filter(category=1)
+	return render(request, 'blog/posts_categorie1.html', {'posts_categorie1':posts_categorie1})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
